5025401e5e8e2330180e1c7e0612eff5c190d847
ShamcyBens <bernardshammah@gmail.com>
2024-11-24 11:30:30 +0300

    updates

--- shamynawebsite/urls.py
+++ shamynawebsite/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from shamynawebsite import views
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.home, name='home'),  # Home page
@@ -7,3 +9,8 @@ urlpatterns = [
     path('right/', views.right, name='right'),  # Right page
 ]
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
